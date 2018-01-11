9d50c7cbda2158b2f2eab2b1306b193586597d12
Slayd7 <brad.chopper@gmail.com>
2018-01-10 18:56:49 -0800

    Animations started
    Almost looks like a game! Basic movement animations are in.

--- Assets/Scripts/Controllers/InputController.cs
+++ Assets/Scripts/Controllers/InputController.cs
@@ -4,17 +4,11 @@ using UnityEngine;
 
 public class InputController : MonoBehaviour {
     
-    [HideInInspector]
-    public float Vertical, Horizontal;
-    
-    [HideInInspector]
-    public Vector2 MouseInput;
-
-    [HideInInspector]
-    public bool Fire1;
-
-    [HideInInspector]
-    public bool Reload;
+    [HideInInspector] public float Vertical, Horizontal;    
+    [HideInInspector] public Vector2 MouseInput;
+    [HideInInspector] public bool Fire1;
+    [HideInInspector] public bool Reload;
+    [HideInInspector] public bool IsWalking, IsSprinting, IsCrouched;
 
     private void Update()
     {
@@ -23,5 +17,8 @@ public class InputController : MonoBehaviour {
         MouseInput = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
         Fire1 = Input.GetButton("Fire1");
         Reload = Input.GetKey(KeyCode.R);
+        IsWalking = Input.GetKey(KeyCode.LeftControl);
+        IsSprinting = Input.GetKey(KeyCode.LeftShift);
+        IsCrouched = Input.GetKey(KeyCode.C);
     }
 }
