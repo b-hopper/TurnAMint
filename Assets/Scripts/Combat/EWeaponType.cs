15964d95119d556be2901fc6e050d728241e0df8
Slayd7 <brad.chopper@gmail.com>
2018-01-14 18:03:36 -0800

    Scrap and restart
    Welp.
    
    Decided that what I had wasn't headed towards the feel I wanted, there was going to be way too much to change and redo. So, it's probably better to start from scratch.

--- Assets/Scripts/Combat/EWeaponType.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public enum EWeaponType {
-    M4A1,
-    Shotgun
-}
---
+++ Assets/Scripts/InputHandler.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputHandler : MonoBehaviour {
+
+    public float horizontal;
+    public float vertical;
+    public float mouse1;
+    public float mouse2;
+    public float fire3;
+    public float middleMouse;
+    public float mouseX;
+    public float mouseY;
+
+    [HideInInspector]
+    public FreeCameraLook camProperties;
+    [HideInInspector]
+    public Transform camPivot;
+    [HideInInspector]
+    public Transform camTrans;
+
+    CrosshairManager crosshairManager;
+    ShakeCamera shakeCam;
+    StateManager states;
+
+    public float normalFov = 60;
+    public float aimingFov = 40;
+    float targetFov;
+    float curFov;
+    public float cameraNormalZ = -2;
+    public float cameraAimingZ = -0.86f;
+    float targetZ;
+    float actualZ;
+    float curZ;
+    LayerMask layerMask;
+
+    public float shakeRecoil = 0.5f;
+    public float shakeMovement = 0.3f;
+    public float shakeMin = 0.1f;
+    float targetShake;
+    float curShake;
+
+    private void Start()
+    {
+        crosshairManager = CrosshairManager.GetInstance();
+        camProperties = FreeCameraLook.GetInstance();
+
+        camPivot = camProperties.transform.GetChild(0);
+        camTrans = camPivot.GetChild(0);
+        shakeCam = camPivot.GetComponentInChildren<ShakeCamera>();
+
+        states = GetComponent<StateManager>();
+
+        layerMask = ~(1 << gameObject.layer);
+        states.layerMask = layerMask;
+    }
+
+    private void FixedUpdate()
+    {
+        HandleInput();
+        UpdateStates();
+        HandleShake();
+
+        // Find where the camera is looking
+        Ray ray = new Ray(camTrans.position, camTrans.forward);
+        states.lookPosition = ray.GetPoint(20);
+    }
+
+    private void HandleShake()
+    {
+        throw new NotImplementedException();
+    }
+
+    private void UpdateStates()
+    {
+        throw new NotImplementedException();
+    }
+
+    private void HandleInput()
+    {
+        throw new NotImplementedException();
+    }
+}
