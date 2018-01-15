cbdf7842b8a7dab07653b871685b12411f62b00a
Slayd7 <brad.chopper@gmail.com>
2018-01-15 00:41:43 -0800

    Rearranging scripts
    Just some housekeeping

---
+++ Assets/Scripts/ObjectPool.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPool : MonoBehaviour
+{
+    public int ObjectsPerElement;
+    public GameObject[] ObjectsInPool;
+    Dictionary<int, GameObject[]> ObjPool;
+    int[] objIndex;
+    int poolIndex;
+
+    private void Start()
+    {
+        ObjPool = new Dictionary<int, GameObject[]>();
+        poolIndex = 0;
+        objIndex = new int[ObjectsInPool.Length];
+        foreach (GameObject obj in ObjectsInPool)
+        {
+            GameObject newPool = new GameObject(obj.name + "_Pool");
+            GameObject[] pool = new GameObject[ObjectsPerElement];
+
+            for (int i = 0; i < ObjectsPerElement; i++)
+            {
+                GameObject tmp = Instantiate(obj);
+                pool[i] = tmp;
+                tmp.SetActive(false);
+                tmp.transform.parent = newPool.transform;                    
+            }
+            ObjPool.Add(poolIndex, pool);
+            poolIndex++;
+        }
+        poolIndex--;
+    }
+
+    public GameObject GetNewObj()
+    {
+        GameObject[] objArray;
+        GameObject obj = null;
+        if (ObjPool.Count > 0)
+        {
+            int selectedIndex = Random.Range(0, poolIndex);
+
+            objArray = ObjPool[selectedIndex];
+
+            obj = objArray[objIndex[selectedIndex]];
+
+            objIndex[selectedIndex] = (objIndex[selectedIndex] + 1) % (ObjectsPerElement);
+
+            obj.SetActive(false);
+            obj.SetActive(true);
+        }
+        return obj;
+    }
+}
