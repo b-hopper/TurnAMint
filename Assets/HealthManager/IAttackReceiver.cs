cccd4a8cfb1915441991a08d17e6e5f2db703ae1
Slayd7 <brad.chopper@gmail.com>
2018-01-14 18:03:36 -0800

    Scrap and restart
    Welp.
    
    Decided that what I had wasn't headed towards the feel I wanted, there was going to be way too much to change and redo. So, it's probably better to start from scratch.

--- Assets/HealthManager/IAttackReceiver.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine.EventSystems;
-
-namespace HealthManagement
-{
-    public interface IAttackReceiver : IEventSystemHandler
-    {
-        void ReceiveAttack(Weapons.Attack attack);
-    }
-}
